e425053732ae0405840d861516e293282096af92
LiorBitton <54041768+LiorBitton@users.noreply.github.com>
2020-09-27 01:00:18 +0300

    Update Main.c

--- Assignment 7 Hospital and linked lists/Assignment 7 Hospital and linked lists/Main.c
+++ Assignment 7 Hospital and linked lists/Assignment 7 Hospital and linked lists/Main.c
@@ -1,31 +1,16 @@
-#include <stdio.h>
-#include "Doctor.h"
-#include "Hospital.h"
-#include "Patient.h"
-#pragma warning(disable : 4996)
-int main() {
-	Hospital* hos = newHospital();
-	Doctor* doc = newDoctor("Lior", "Physiotherapy");
-	Doctor* doc1 = newDoctor("Moshe", "Surgeon");
-	Doctor* doc2 = newDoctor("Itzik", "Neurosurgeon");
-	Doctor* doc3 = newDoctor("Ofir", "Psychiatrist");
-	Patient* pat = newPatient("Lior", 2004, doc);
-	Patient* pat1 = newPatient("Ofir", 2004, doc2);
-	printPatient(pat);
-	printf("Changed Lior's attending doctor\n");
-	setAttendingDoctor(pat, doc1);
-	printPatient(pat);
-	addDoctor(hos, doc);
-	addDoctor(hos, doc1);
-	addDoctor(hos, doc2);
-	addDoctor(hos, doc3);
-	addPatient(hos, pat);
-	addPatient(hos, pat1);
-	printHospital(hos);
-	removePatient(hos, pat1);
-	printf("Fired Lior and sent Ofir home\n");
-	fireDoctor(hos, doc);
-	printHospital(hos);
-	freeHospital(hos);
-	return 0;
+
+
+void fill(int** matrix) {
+	int arr[2][2] = { {1,2},{3,4} };
+	for (int i = 0; i < 2;i++)
+		for (int j = 0; j < 2;j++)
+			matrix[i][j] = arr[i][j];
 }
+	int main() {
+		int arr[2][2];
+		fill(arr); 
+		f(2);
+	}
+	int f(int x) {
+		return 0;
+	}
